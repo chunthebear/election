70e0ea4fb1111705a44acbfb81c8a406dab216cf
chunthebear <zyc5026@hotmail.com>
2019-01-15 15:11:09 -0800

    Added voting functionality

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,28 +1,40 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 contract Election{
 
-	//to model a candidate
-	struct Candidate{
-		uint id;
-		string name;
-		uint voteCount;
-	}
-	
-	//to store candidates
-	//candidate ID mapped to candidate
-	mapping (uint => Candidate) public candidates;
+    //to model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    
+    //to store candidates
+    //candidate ID mapped to candidate
+    mapping (uint => Candidate) public candidates;
 
-	uint public candidateCount;
+    uint public candidateCount;
 
-	function Election() public{
-		addCandidate("Candidate 1");
-		addCandidate("Candidate 2");
-	}
+    constructor() public {
+        addCandidate("C1");
+        addCandidate("C2");
+    }
 
-	//underscore means local variable, not state variable
-	function addCandidate(string _name) private{
-		candidateCount++;
-		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
-	}
+    //underscore means local variable, not state variable
+    function addCandidate(string memory _name) private{
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    }
+
+    function getCandidate(uint id) public view returns(string memory){
+        return string(abi.encodePacked("Name: ",candidates[id].name);
+    }
+
+	function getVote(uint id) public view returns(uint){
+        return candidates[id].voteCount;
+    }
+
+    function addVote(uint id) private{
+        candidates[id].voteCount++;
+    }
 }
